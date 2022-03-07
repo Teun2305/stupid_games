905e7318d8243fa66a119cdcaa925f21d998d50c
Hanraets, T.J.J. (Teun) <teun.hanraets@student.ru.nl>
2022-03-07 10:52:09 +0100

    added PlayerInterface

--- game.py
+++ game.py
@@ -1,6 +1,26 @@
 BOARD_SIZE = 3
 
 class PlayerInterface:
-    def __init__(self, name, symbol):
-        self.name = name
-        self.symbol = symbol
+    def __init__(self, symbol):
+        self.__name = self.set_name()
+        self.__symbol = symbol
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self):
+        pass
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def play(self):
+        pass
+
+    def __str__(self):
+        return self.__symbol
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__symbol == other.get_symbol()
+        return False
