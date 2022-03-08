3650fbc336cb70cc8dc13726b4012dd24e4ac777
Hanraets, T.J.J. (Teun) <teun.hanraets@student.ru.nl>
2022-03-08 21:54:03 +0100

    removed bugs concerning game logic

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import numpy as np
 from random import  randint
+from time import sleep
 
 BOARD_SIZE = 4
 GAME_N = 3
@@ -33,8 +34,8 @@ class PlayerInterface:
 
 class HumanPlayer(PlayerInterface):
     def set_name(self):
-        return input(f"Player {self._symbol}, what is your name? >> ")
-        # return "Teun"
+        #return input(f"Player {self._symbol}, what is your name? >> ")
+        return "Teun"
 
     def play(self):
         # Can raise ValueError
@@ -53,7 +54,8 @@ class MiniMaxPlayer(PlayerInterface):
         TODO: add call to MiniMax algorithm
         TODO: write MiniMax algorithm
         """
-        return randint(1, BOARD_SIZE**2)
+        sleep(randint(5, 10)*0.1)
+        return randint(1, BOARD_SIZE), randint(1, BOARD_SIZE)
 
 
 class Board:
@@ -146,7 +148,7 @@ class Board:
     
         # Top right to bottom left
         for row in range(BOARD_SIZE - GAME_N + 1):
-            for column in range(BOARD_SIZE - GAME_N + 1, BOARD_SIZE):
+            for column in range(BOARD_SIZE - GAME_N + 2, BOARD_SIZE):
                 array = []
                 for i in range(GAME_N):
                     array.append(self.__board[row + i][column - i])
@@ -190,6 +192,13 @@ class Board:
         row, column = selected_square
         return self.__board[row][column] == 0
     
+    def is_full(self):
+        for row in self.__board:
+            for column in row:
+                if column == 0:
+                    return False
+        return True
+    
     def __str__(self): 
         t = u"\u2550"*3
         l = u"\u2551"
@@ -208,34 +217,72 @@ class Board:
             if i < BOARD_SIZE - 1:
                 string += u"\u2560" + (t + u"\u256C")*(BOARD_SIZE - 1) + t + u"\u2563\n"
         # Bottom Row        
-        string += u"\u255A" + (t + u"\u2569")*(BOARD_SIZE - 1) + t + u"\u255D\n"    
+        string += u"\u255A" + (t + u"\u2569")*(BOARD_SIZE - 1) + t + u"\u255D"    
         return string
     
 class Game:
     def __init__(self):
         self.__player1 = HumanPlayer('X')
-        self.__player2 = MiniMaxPlayer('O')
+        #self.__player2 = MiniMaxPlayer('O')
+        self.__player2 = HumanPlayer('O')
         self.__board = Board()
-        self.__game_over = False
-        self.__player_won = None
         
-    def play_game(self):
-        print('The game is about to start, do you want to read through the rules first?', end=' ')
-        if self.__yes_no_input() == 'y':
-            print('Just google them then')
-            
-        while not self.__game_over:
-            pass
-        
-    def yes_no_input(self):
+    def play_game(self):        
+        player_turn = self.__player1
+        while not self.__board.is_full():
+            print(">"*10, "<"*10, sep="")
+            print(f"{player_turn.get_name()}, it is your turn.")
+            print(self.__board)
+            turn = self.__get_turn(player_turn)
+            self.__board.play_board(turn, player_turn)
+            if self.__board.is_winner(player_turn):
+                break
+            elif player_turn == self.__player1:
+                player_turn = self.__player2
+            else:
+                player_turn = self.__player1
+        print(self.__board)
+        if self.__board.is_full():
+            print('It\'s a draw!!!')
+        else:
+            print(f"Congratulations {player_turn.get_name()}, you have won!")
+    
+    def __get_turn(self, player):
         try:
-            response = input('[y/n] >> ').lower()
-            assert response == 'y' or response == 'n'
-            return response
+            turn = player.play()
+            row, column = turn
+            assert row >= 0 and row < BOARD_SIZE
+            assert column >= 0 and column < BOARD_SIZE
+            assert self.__board.move_is_valid(turn)
+            return row, column
+        except ValueError:
+            print('Please enter a NUMBER.')
+            return self.__get_turn(player)
         except AssertionError:
-            print('Invalid input, please enter y or n.')
-            return self.__yes_no_input()
+            print(f'Please enter a number between 0 and {BOARD_SIZE**2 + 1}.')
+            print('If a square is already taken, you cannot play it again.')
+            return self.__get_turn(player)
+                        
+def yes_no_input():
+    try:
+        response = input('[y/n] >> ').lower()
+        assert response == 'y' or response == 'n'
+        return response
+    except AssertionError:
+        print('Invalid input, please enter y or n.')
+        return yes_no_input()
             
 
 if __name__ == '__main__':
-    pass
+    assert GAME_N <= BOARD_SIZE
+    print("Welcome to Tic Tac No!!!")    
+    print('The game is about to start, do you want to read through the rules first?', end='')
+    if yes_no_input() == 'y':
+        print('Just google them then.')
+    while True:
+        game = Game()
+        game.play_game()
+        print("Do you want to play another game?", end="")
+        if yes_no_input() == 'n':
+            break
+    print("Fuck you!")
